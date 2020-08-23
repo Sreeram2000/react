a5e0e0f07e2cf3ed72c6283e1e1714dfc5e56f7c
Sreeram2000 <sreeramshenai2000@gmail.com>
2020-08-23 23:51:26 +0530

    commit 2

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Greet from './components/Greet' 
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Hello />
+      </div>
+    );
+  }
 }
 
 export default App;
