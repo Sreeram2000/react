9f388e302f94d4b6b911d5904e792686680de429
Sreeram2000 <sreeramshenai2000@gmail.com>
2020-08-24 21:48:13 +0530

    props vs state

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,12 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet' 
 import Welcome from './components/Welcome'
+import Message from './components/Message'
 import Hello from './components/Hello'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Hello />
+      {//<Greet name="Bruce" heroName="Batman"/>
+      // <Greet name="Clark" heroName="Superman"/>
+      //<Greet name="Diana" heroName="Wonder Woman"/>
+      }
+      <Message />
       </div>
     );
   }
