33666553f0dda41d29aadb15c365631d8582fbf9
Sreeram2000 <sreeramshenai2000@gmail.com>
2020-08-24 22:07:47 +0530

    commit 3

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,16 +4,20 @@ import './App.css';
 import Greet from './components/Greet' 
 import Welcome from './components/Welcome'
 import Message from './components/Message'
+import Counter from './components/Counter'
 import Hello from './components/Hello'
 class App extends Component {
   render() {
     return (
       <div className="App">
-      {//<Greet name="Bruce" heroName="Batman"/>
+      {<Greet name="Bruce" heroName="Batman"/>
       // <Greet name="Clark" heroName="Superman"/>
-      //<Greet name="Diana" heroName="Wonder Woman"/>
+      // <Greet name="Diana" heroName="Wonder Woman"/>
       }
-      <Message />
+      <Welcome name="Clark" heroName="Superman"/>
+      {//<Message />
+      }
+      <Counter />
       </div>
     );
   }
--- hello-world/src/components/Welcome.js
+++ hello-world/src/components/Welcome.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 
 class Welcome extends Component {
     render() {
-        return <h1>Class Component</h1>
+        const {name,heroName} = this.props
+        return <h1>{name} a.k.a {heroName}</h1>
     }
 }
 
