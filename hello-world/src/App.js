f4f22cc2133e831c76c414cb2606b67decaedc28
Sreeram2000 <sreeramshenai2000@gmail.com>
2020-08-30 22:44:33 +0530

    Form Handling

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -16,17 +16,19 @@ import StyleSheet from './components/StyleSheet';
 import Inline from './components/Inline';
 import './appStyles.css'
 import styles from './appStyles.module.css' 
+import Form from './components/Form';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       
-      <h1 className="error">Error</h1>
-      <h1 className={styles.success}>Success</h1>
+      <Form />
 
       {//<ParentComponent /><StyleSheet primary={true} />
-       //<EventBind />
+       //<EventBind /> 
+       //<h1 className="error">Error</h1>
+       //<h1 className={styles.success}>Success</h1>
        //<NameList />
        //<UserGreeting /><Inline />
       }
