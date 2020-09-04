a797ed1f8f0a4774ae8412211ced890774c11ca0
Sreeram2000 <sreeramshenai2000@gmail.com>
2020-09-04 23:25:10 +0530

    Refs with class components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
+import Focusinput from './components/Focusinput';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <RefsDemo />
+        <Focusinput />
       </div>
     );
   }
