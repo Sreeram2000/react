c2fd1c349606fe6220923270a5ab186beb861958
Sreeram2000 <sreeramshenai2000@gmail.com>
2020-11-03 20:52:08 +0530

    date-builder-set

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,6 +3,19 @@ const api ={
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
+const dateBuilder = (d) => {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`
+}
+
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +23,10 @@ function App() {
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Search..."/> 
         </div>
+        <div className="location-box">
+            <div className="location">Chennai</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
       </main>
     </div>
   );
