49786a08f1018a349383ceec51300b9024610d20
Sreeram2000 <sreeramshenai2000@gmail.com>
2020-11-03 20:48:21 +0530

    css-edit-1

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,7 +6,11 @@ const api ={
 function App() {
   return (
     <div className="App">
-      Hello
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..."/> 
+        </div>
+      </main>
     </div>
   );
 }
