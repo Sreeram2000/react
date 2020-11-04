52b342d3fcbfb9bf17b9df3a92c251a46431af3d
Sreeram2000 <sreeramshenai2000@gmail.com>
2020-11-04 18:59:31 +0530

    fetch-api

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,7 +1,25 @@
+import React, { useState } from 'react';
+
 const api ={
   key:"8be821008540d277fcd2d241a5bbffd0",
   base: "https://api.openweathermap.org/data/2.5/"
 }
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
 
 const dateBuilder = (d) => {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
